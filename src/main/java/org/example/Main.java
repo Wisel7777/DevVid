0b27142f8303fc2c655c52e616f7cf6d302a41fa
Wisel7777 <wisemoussa@gmail.com>
2024-03-23 00:57:21 +0000

    good

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num1 = 5;
+        int num2 = 3;
+
+        int sum = add(num1, num2);
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
-}
+
+    public static int add(int a, int b) {
+        return a + b;
+    }}
